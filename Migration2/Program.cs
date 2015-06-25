5e7fdd3efe844d1ac65df6d609fbdc8aac180b99
olich <glossycode@me.com>
2015-06-25 14:37:45 +0200

    INIT

---
+++ Migration2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using Migration2.Migrations;
+
+namespace Migration2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BlogContext, Configuration>()); 
+ 
+            using (var db = new BlogContext())
+            {
+                db.Blogs.Add(new Blog { Name = "Another Blog " });
+                db.SaveChanges();
+
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(blog.Name);
+                }
+            }
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(); 
+        }
+    }
+}
